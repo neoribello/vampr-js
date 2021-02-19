eb92bd6b41becf161078c4381b0d0932e931100f
Neil Oribello <neil.oribello@gmail.com>
2021-02-19 20:31:20 +0000

    passed the tests for tree methods

--- vampire.js
+++ vampire.js
@@ -10,22 +10,36 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
+    let numberOfVamps = 0;
+    let currentVamp = this;
+
+    while (currentVamp.creator) {
+      currentVamp = currentVamp.creator;
+      numberOfVamps++;
+    }
+    return numberOfVamps;
 
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    for(let i = 0; i < this.numberOfOffspring; i++){
+      if(vampire.name === this.offspring[i].name) {
+        return true;
+      }
+    }
+    return false;
   }
 
   /** Stretch **/
